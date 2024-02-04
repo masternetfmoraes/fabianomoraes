1ff466085c0e5f60627fe36f64a031bec00bd5bb
masternetfmoraes <masternet.fabiano@gmail.com>
2024-02-04 01:38:29 -0300

    assyncr

--- src/app/api/page.tsx
+++ src/app/api/page.tsx
@@ -1,29 +1,28 @@
-import nodemailer from 'nodemailer';
+const nodemailer = require('nodemailer');
+//const config = require('../../../config.local')
+export default function sendEmail(req: any, response: any){
 
-export default async function sendEmail(req: any, response: any) {
-  try {
     let transporter = nodemailer.createTransport({
       service: 'hotmail',
       auth: {
         user: 'moraes_18@hotmail.com',
         pass: 'senha123',
       },
-    });
-
+    })
     const mailOptions = {
       from: 'moraes_18@hotmail.com',
       to: 'moraes_18@hotmail.com',
       subject: 'Assunto do e-mail',
       text: 'Tesxto do email',
       html: `<b>Hello world?</b>
-      <b>Teste de Email<b><hr /> <h1>Teste</h1><br /><h6>Teste de h6</h6>`,
+      <b>Teste de Email<b><hr /> <h1>Teste</h1><br /><h6>Teste de h6</h6>
+      `, // html body
     };
-
-    const info = await transporter.sendMail(mailOptions);
-    console.log('E-mail enviado: ' + info.response);
-    response.status(200).json({ message: 'E-mail enviado com sucesso!' });
-  } catch (error) {
-    console.error(error);
-    response.status(500).json({ error: 'Erro ao enviar o e-mail' });
-  }
+    transporter.sendMail(mailOptions, function (error: any, info: { response: string; }) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('E-mail enviado: ' + info.response);
+      }
+    });
 }
